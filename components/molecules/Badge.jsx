5bea095fb2178ef537e71e65cab77cbc34fb4a20
afifudinx <afifudin.fdn@gmail.com>
2022-12-22 14:47:55 +0700

    Add Badge

---
+++ components/molecules/Badge.jsx
@@ -0,0 +1,11 @@
+export default function Badge({ className, name }) {
+  return (
+    <div
+      className={`${
+        className ? className + " " : ""
+      }text-xs font-medium whitespace-nowrap text-green-500 bg-green-100 px-2 py-0.5 rounded`}
+    >
+      {name}
+    </div>
+  );
+}
