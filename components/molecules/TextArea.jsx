37c07b4dcbebcd8a51061e3996968e4cb764ef91
afifudinx <work.afifudin@gmail.com>
2022-12-25 12:23:07 +0700

    Adding TextArea

---
+++ components/molecules/TextArea.jsx
@@ -0,0 +1,25 @@
+export default function TextArea({
+  className,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) {
+  const doChange = (e) => {
+    onChange({ name, value: e.currentTarget.value });
+  };
+
+  return (
+    <textarea
+      className={`${
+        className ? className + " " : ""
+      }text-xs font-medium w-full p-4 rounded-lg shadow-custom outline-none bg-white h-48 resize-none`}
+      maxLength={250}
+      onChange={doChange}
+      name={name}
+      id={name}
+      value={value}
+      placeholder={placeholder}
+    ></textarea>
+  );
+}
