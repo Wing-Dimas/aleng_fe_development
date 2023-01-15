5ea777587ff85ccb5a2fa2293472df4b59e9d82c
afifudinx <work.afifudin@gmail.com>
2023-01-15 08:08:53 +0700

    update .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    BASE_API: "http://api.lenjelenanmadura.id/api",
+  },
 };
 
 module.exports = nextConfig;
