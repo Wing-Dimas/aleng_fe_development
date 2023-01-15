f83a4a8a8a4d167e7df8ac6f5579d35736d49db9
afifudinx <work.afifudin@gmail.com>
2023-01-15 11:52:48 +0700

    Adding validators

---
+++ validators/resetPasswordValidator.js
@@ -0,0 +1,31 @@
+const { object, string, ref } = require("yup");
+
+let loginSchema = object().shape({
+  password: string("Password tidak valid")
+    .min(8, "Password harus lebih dari 8 karakter")
+    .required("Password tidak boleh kosong"),
+  password_confirmation: string("Password tidak valid").oneOf(
+    [ref("password"), null],
+    "Password tidak sama"
+  ),
+});
+
+const validateResetPassword = async (form) => {
+  try {
+    const res = await loginSchema.validate(form, { abortEarly: false });
+    // return back form
+    return { isError: false, form };
+  } catch (err) {
+    let paths = [];
+    let errors = {};
+    err.inner.forEach((e) => {
+      if (!paths.includes(e.path)) {
+        paths.push(e.path);
+        errors[e.path] = { isError: true, message: e.message };
+      }
+    });
+    return { isError: true, form: errors };
+  }
+};
+
+export default validateResetPassword;
