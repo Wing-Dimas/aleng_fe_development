1100efca8d4e520f70a9108aae50d7d6c3bfe307
afifudinx <work.afifudin@gmail.com>
2023-01-17 09:05:14 +0700

    configure nextauth

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     BASE_API: "http://api.lenjelenanmadura.id/api",
+    GOOGLE_CLIENT_ID:
+      "281996152491-vvhjselprkkeehk4u39rmmh92emjckkr.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
+    NEXTAUTH_URL: "http://localhost:3000",
   },
 };
 
