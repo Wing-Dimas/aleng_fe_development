6204da9071bfc6a1837ba1b6680e7e1991a30898
afifcodes <work.afifudin@gmail.com>
2023-11-04 11:57:48 +0700

    Edit MOCK_API

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     BASE_API: "https://server.lenjelenanmadura.id/api",
     BASE_STORAGE: "https://server.lenjelenanmadura.id/storage/",
-    MOCK_API: "http://localhost:8000/api",
+    MOCK_API: "https://raw.githubusercontent.com/afifcodes/sample-api/main/sample",
     JWT_SECRET:
       "QwGJkFcp3bzNz242mtmy4zrgROBTGE0A6GuApawpAxI1XsHMO6wVIKq9MT26G5vT",
   },
